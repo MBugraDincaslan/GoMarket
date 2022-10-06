c5ad89bf3081525e4f3d59831636700975904b62
Bugra Dincaslan <bugra.dincaslan@obss.com.tr>
2022-10-06 16:20:42 +0300

    Initial Commit

---
+++ GoMarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoMarket
+//
+//  Created by obss on 6.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
